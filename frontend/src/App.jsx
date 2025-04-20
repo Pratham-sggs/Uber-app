1f4d2ad67aca06a050993cc51f7bf874a753fa55
Pratham-sggs <2022bit045@sggs.ac.in>
2025-04-21 02:35:19 +0530

    Created Riding UI for Captain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import UserLogout from './pages/UserLogout';
 import CaptainHome from './pages/CaptainHome';
 import CaptainLogout from './pages/CaptainLogout';
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper';
+import Riding from './pages/Riding';
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="/riding" element={<Riding />} />
         <Route path="/signup" element={< UserSignup />} />
         <Route path="/captain-signup" element={< CaptainSignup />} />
         <Route path="/captain-login" element={< CaptainLogin />} />
